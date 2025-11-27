533444770a5623e8407206d4665a53ca2f875cc9
notonly-clayde <clyde04limpangog@gmail.com>
2025-11-27 23:39:15 +0800

    feat(report): lost and found routes

--- server/src/routes/report/index.ts
+++ server/src/routes/report/index.ts
@@ -7,5 +7,12 @@ const router = Router();
 // found
 router.post("/found/v1", authenticate, ReportController.addFoundItem)
 router.get("/found/v1", authenticate, ReportController.getFoundItems)
+router.get("/found/v1/:id", authenticate, ReportController.getFoundItem)
+// router.put("/found/v1/:id", authenticate, ReportController.updateFoundItem)
+
+// lost
+router.post("/lost/v1", authenticate, ReportController.addLostItem)
+router.get("/lost/v1", authenticate, ReportController.getLostItems)
+router.get("/found/v1/:id", authenticate, ReportController.getLostItem)
 
 export default router;
