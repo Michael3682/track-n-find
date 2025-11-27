ceed5374aaf0d76e93435c32e614787cbfcccd64
= <=>
2025-11-28 01:06:11 +0800

    feat(browse): synced cards from db

--- client/lib/reportService.ts
+++ client/lib/reportService.ts
@@ -16,3 +16,50 @@ export const reportFound = async ({itemName, date, time, location, description, 
        return [null, err];
     }
 }
+
+export const reportLost = async ({itemName, date, time, location, description, attachments}: {itemName: string, date: string, time: string, location: string, description: string, attachments?: string[], userId: string}) => {
+    try {
+       const res = await fetch(`${API_URL}/report/lost/v1`, {
+          method: "POST",
+          credentials: "include",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ itemName, date, time, location, description, attachments }),
+       });
+       const data = await res.json();
+
+       return [data, null];
+    } catch (err) {
+       console.log(err);
+       return [null, err];
+    }
+}
+
+export const getFoundItems = async () => {
+   try {
+      const res = await fetch(`${API_URL}/report/found/v1`, {
+         method: "GET",
+         credentials: "include"
+      })
+      const data = await res.json()
+
+      return [data, null]
+   } catch (err) {
+       console.log(err);
+       return [null, err];
+    }
+}
+
+export const getLostItems = async () => {
+   try {
+      const res = await fetch(`${API_URL}/report/lost/v1`, {
+         method: "GET",
+         credentials: "include"
+      })
+      const data = await res.json()
+
+      return [data, null]
+   } catch (err) {
+       console.log(err);
+       return [null, err];
+    }
+}
