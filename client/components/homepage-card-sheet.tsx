9ff00df203728c70e7a8ce45fef68be6bb615836
= <=>
2025-12-12 14:49:33 +0800

    fix(cards): homepage cards are now synced

--- client/components/homepage-card-sheet.tsx
+++ client/components/homepage-card-sheet.tsx
@@ -33,6 +33,7 @@ import {
     CarouselPrevious,
     CarouselApi,
 } from "@/components/ui/carousel"
+import CardSheet from "./card-sheet";
 
 export function HomepageCardSheet() {
     const isMobile = useIsMobile();
@@ -65,12 +66,6 @@ export function HomepageCardSheet() {
        });
     };
 
-    const handleMessageUser = async (item: Item) => {
-        const [data] = await findOrCreateConversation({ itemId: item.id, hostId: item.author.id })
-
-        router.push(`/messages/${data.conversation.id}`)
-    }
-
     useEffect(() => {
         getItems().then(([data]) => setItems(data.items));
 
@@ -99,102 +94,7 @@ export function HomepageCardSheet() {
                     <CarouselContent className="-ml-5">
                         {foundItems.map((item) => (
                             <CarouselItem key={item.id} className="pl-5 basis-full md:basis-1/2 lg:basis-1/3 xl:basis-1/5">
-                                <Sheet>
-                                    <SheetTrigger className="cursor-pointer p-0" asChild>
-                                        <Card className="w-full flex gap-0 bg-transparent overflow-hidden border rounded-sm hover:border-black/25 transition-all duration-100 ease-linear">
-                                            <CardHeader className="bg-primary-foreground p-0 gap-0 relative">
-                                                <CardTitle>
-                                                    <img
-                                                        className="aspect-video lg:h-50 object-cover object-center z-10"
-                                                        src={
-                                                            item?.attachments?.length > 0
-                                                                ? item.attachments[0]
-                                                                : undefined
-                                                        }
-                                                        alt="image"
-                                                    />
-                                                    <Badge
-                                                        className={`${item.status === "CLAIMED"
-                                                            ? "bg-green-400"
-                                                            : "bg-red-400"
-                                                            } z-20 absolute top-0 right-0 rounded-tl-none rounded-tr-none rounded-bl-md rounded-br-none`}>
-                                                        <small>{item.status}</small>
-                                                    </Badge>
-                                                </CardTitle>
-                                            </CardHeader>
-                                            <CardDescription className="p-4 text-medium font-medium lg:text-base text-primary flex flex-col">
-                                                {item.name}
-                                            </CardDescription>
-                                        </Card>
-                                    </SheetTrigger>
-                                    <SheetContent className="p-8 lg:p-12" side="center">
-                                        <SheetHeader className="p-0 space-y-3 lg:space-y-5">
-                                            <img
-                                                className="aspect-video object-contain object-top shadow-inner shadow-black/10 rounded-md p-5 drop-shadow-lg drop-shadow-black/50"
-                                                src={
-                                                    item?.attachments?.length > 0
-                                                        ? item.attachments[0]
-                                                        : undefined
-                                                }
-                                                alt="image"
-                                            />
-                                            <div className="space-y-7">
-                                                <SheetTitle className="text-xl lg:text-3xl">
-                                                    {item.name}
-                                                    <p className="text-xs font-light text-muted-foreground">
-                                                        Reported By:{" "}
-                                                        <span className="font-normal">
-                                                            {item.author.name}
-                                                        </span>
-                                                    </p>
-                                                </SheetTitle>
-                                                <div className="space-y-7">
-                                                    <p className="text-base lg:text-lg text-muted-foreground">
-                                                        {item.description}
-                                                    </p>
-                                                    <div className="space-y-2">
-                                                        <Separator />
-                                                        <p className="text-xs lg:text-base font-medium text-primary flex justify-between">
-                                                            Reported on:{" "}
-                                                            <span className="font-normal">
-                                                                {formattedDate(item.date_time)}
-                                                            </span>
-                                                        </p>
-                                                        <p className="text-xs lg:text-base font-medium text-primary flex justify-between">
-                                                            Location:{" "}
-                                                            <span className="font-normal">
-                                                                {item.location}
-                                                            </span>
-                                                        </p>
-                                                        <p className="text-xs lg:text-base font-medium text-primary flex justify-between">
-                                                            Status:{" "}
-                                                            <span className="font-semibold text-red-500">
-                                                                {item.status}
-                                                            </span>
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </SheetHeader>
-                                        <SheetFooter className="px-0">
-                                            <Button
-                                                className="text-xs lg:text-base py-0 lg:py-5 cursor-pointer"
-                                                type="submit"
-                                                asChild>
-                                                {
-                                                    item.associated_person == user?.id ?
-                                                        <Link href={`update/${item.id}`}>
-                                                            Manage Item
-                                                        </Link>
-                                                        :
-                                                        <p onClick={() => handleMessageUser(item)}>
-                                                            Message User
-                                                        </p>
-                                                }
-                                            </Button>
-                                        </SheetFooter>
-                                    </SheetContent>
-                                </Sheet>
+                                <CardSheet item={item}/>
                             </CarouselItem>
                         ))}
                     </CarouselContent>
@@ -226,102 +126,7 @@ export function HomepageCardSheet() {
                     <CarouselContent className="-ml-5">
                         {lostItems.map((item) => (
                             <CarouselItem key={item.id} className="pl-5 basis-full md:basis-1/2 lg:basis-1/3 xl:basis-1/5">
-                                <Sheet>
-                                    <SheetTrigger className="cursor-pointer p-0" asChild>
-                                        <Card className="w-full flex gap-0 bg-transparent overflow-hidden border rounded-sm hover:border-black/25 transition-all duration-100 ease-linear">
-                                            <CardHeader className="bg-primary-foreground p-0 gap-0 relative">
-                                                <CardTitle>
-                                                    <img
-                                                        className="aspect-video lg:h-50 object-cover object-center z-10"
-                                                        src={
-                                                            item?.attachments?.length > 0
-                                                                ? item.attachments[0]
-                                                                : undefined
-                                                        }
-                                                        alt="image"
-                                                    />
-                                                    <Badge
-                                                        className={`${item.status === "CLAIMED"
-                                                            ? "bg-green-400"
-                                                            : "bg-red-400"
-                                                            } z-20 absolute top-0 right-0 rounded-tl-none rounded-tr-none rounded-bl-md rounded-br-none`}>
-                                                        <small>{item.status}</small>
-                                                    </Badge>
-                                                </CardTitle>
-                                            </CardHeader>
-                                            <CardDescription className="p-4 text-medium font-medium lg:text-base text-primary flex flex-col">
-                                                {item.name}
-                                            </CardDescription>
-                                        </Card>
-                                    </SheetTrigger>
-                                    <SheetContent className="p-8 lg:p-12" side="center">
-                                        <SheetHeader className="p-0 space-y-3 lg:space-y-5">
-                                            <img
-                                                className="aspect-video object-contain object-top shadow-inner shadow-black/10 rounded-md p-5 drop-shadow-lg drop-shadow-black/50"
-                                                src={
-                                                    item?.attachments?.length > 0
-                                                        ? item.attachments[0]
-                                                        : undefined
-                                                }
-                                                alt="image"
-                                            />
-                                            <div className="space-y-7">
-                                                <SheetTitle className="text-xl lg:text-3xl">
-                                                    {item.name}
-                                                    <p className="text-xs font-light text-muted-foreground">
-                                                        Reported By:{" "}
-                                                        <span className="font-normal">
-                                                            {item.author.name}
-                                                        </span>
-                                                    </p>
-                                                </SheetTitle>
-                                                <div className="space-y-7">
-                                                    <p className="text-base lg:text-lg text-muted-foreground">
-                                                        {item.description}
-                                                    </p>
-                                                    <div className="space-y-2">
-                                                        <Separator />
-                                                        <p className="text-xs lg:text-base font-medium text-primary flex justify-between">
-                                                            Reported on:{" "}
-                                                            <span className="font-normal">
-                                                                {formattedDate(item.date_time)}
-                                                            </span>
-                                                        </p>
-                                                        <p className="text-xs lg:text-base font-medium text-primary flex justify-between">
-                                                            Location:{" "}
-                                                            <span className="font-normal">
-                                                                {item.location}
-                                                            </span>
-                                                        </p>
-                                                        <p className="text-xs lg:text-base font-medium text-primary flex justify-between">
-                                                            Status:{" "}
-                                                            <span className="font-semibold text-red-500">
-                                                                {item.status}
-                                                            </span>
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </SheetHeader>
-                                        <SheetFooter className="px-0">
-                                            <Button
-                                                className="text-xs lg:text-base py-0 lg:py-5 cursor-pointer"
-                                                type="submit"
-                                                asChild>
-                                                {
-                                                    item.associated_person == user?.id ?
-                                                        <Link href={`update/${item.id}`}>
-                                                            Manage Item
-                                                        </Link>
-                                                        :
-                                                        <p onClick={() => handleMessageUser(item)}>
-                                                            Message User
-                                                        </p>
-                                                }
-                                            </Button>
-                                        </SheetFooter>
-                                    </SheetContent>
-                                </Sheet>
+                                <CardSheet item={item}/>
                             </CarouselItem>
                         ))}
                     </CarouselContent>
--- server/src/services/logs/index.ts
+++ server/src/services/logs/index.ts
@@ -1,11 +1,37 @@
 import LogsRepository from "@/repositories/logs"
+import { Logs } from "@prisma/client"
 
 class LogsService {
     async record({
-        
-    }) {
+        actorId,
+        actorName,
+        action,
+        target,
+        targetId,
+        metaData
+    }: Omit<Logs, "id" | "createdAt">) {
 
     }
 }
 
 export default new LogsService()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+    |  userId  |  name  |  email  |  role  |  date registered  |  
+
+*/
